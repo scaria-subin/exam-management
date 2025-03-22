e413df4099ec73549d8e2dcd913171cd30bbeb25
scaria-subin <scaria.subin@gmail.com>
2025-03-22 13:49:56 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Exam Management System</h1>
+      <p>Welcome to the online exam scheduling and seat allotment system.</p>
+    </div>
+  );
+}
+
+export default App;
